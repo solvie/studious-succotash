92ce202e5d163e5258af43dca7ec8d631eae6da1
Solvie <lee.solvie@gmail.com>
2021-06-15 20:24:41 -0400

    tooltip

--- src/dash.py
+++ src/dash.py
@@ -20,11 +20,26 @@ for n, row in enumerate(z):
 
 
 colorscale = [[0, '#3D9970'], [1, '#001f3f']]  # custom colorscale
-trace = go.Heatmap(x=x, y=y, z=z, colorscale=colorscale, showscale=False)
+trace = go.Heatmap(
+    x=x, 
+    y=y, 
+    z=z, 
+    colorscale=colorscale, 
+    showscale=False,
+    hovertemplate =
+    '<i>Price</i>: %{y}'+
+    '<br><b>Foo</b>: %{x}<br>'+
+    '<b>Bar: %{z}</b>'+
+    '<extra></extra>'
+)
 
-fig = go.Figure(data=trace)
+fig = go.Figure(
+    data=trace,
+)
 fig['layout'].update(
     title="Annotated Heatmap",
+    xaxis=dict(title='something'),
+    yaxis=dict(title='another thing'),
     annotations=annotations,
     width=700,
     height=700,
